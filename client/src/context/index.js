30ff75c942bf94306e41b8b52c0d04ef5b125230
GuysRGithub <guysdeveloperrobot@gmail.com>
2023-02-09 10:29:21 +0700

    Add basic state context

---
+++ client/src/context/index.js
@@ -0,0 +1,47 @@
+import { useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import Web3Modal from "web3modal"
+import { ethers } from "ethers";
+import { ABI, CONTRACT_ADDRESS } from "../contract";
+
+const StateContext = useContext()
+
+export const StateContextProvider = ({ children }) => {
+
+    const navigate = useNavigate();
+
+    const [walletAddress, setWalletAddress] = useState('');
+    const [contract, setContract] = useState('');
+    const [provider, setProvider] = useState('');
+
+
+    const updateCurrentWalletAddress = async () => {
+        const accounts = await window?.ethereum?.request({ method: 'eth_requestAccounts' });
+
+        if (accounts) setWalletAddress(accounts[0]);
+    }
+
+    useEffect(() => {
+        updateCurrentWalletAddress();
+
+        window?.ethereum?.on('accountsChanged', updateCurrentWalletAddress)
+
+        // Set contract and provider
+        const setContractAndProvider = async () => {
+            const web3Model = new Web3Modal();
+            const connection = web3Model.connect();
+            const newProvider = ethers.providers.Web3Provider(connection);
+            const signer = newProvider.getSigner();
+            const newContract = new ethers.Contract(CONTRACT_ADDRESS, ABI, signer);
+
+            setProvider(newProvider);
+            setContract(newContract);
+        }
+
+        setContractAndProvider();
+    }, [])
+
+    return <StateContext.Provider value={{ contract, provider, walletAddress }}>
+        {children}
+    </StateContext.Provider>
+}
---
+++ client/src/contract/index.js
@@ -0,0 +1,4 @@
+import contract from './AVAXGods.json'
+
+export const CONTRACT_ADDRESS = '0xA7a7e04A4989C42b776158596a5b58bB201B888d'
+export const { abi: ABI } = contract

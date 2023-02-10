9fba735ecb0c058215e074efd6426974bdb3221e
GuysRGithub <guysdeveloperrobot@gmail.com>
2023-02-10 16:12:57 +0700

    Add join battles items

--- client/src/page/JoinBattle.jsx
+++ client/src/page/JoinBattle.jsx
@@ -3,13 +3,38 @@ import PageContainer from './PageContainer';
 import { CustomButton, InputField } from '../components';
 import { useNavigate } from 'react-router-dom';
 import styles from '../styles';
+import { useStateContext } from '../context';
 
 const JoinBattle = () => {
   const navigate = useNavigate()
+  const { gameState, setBattleName, walletAddress, contract, setShowAlert } = useStateContext()
+
+  const handleClick = async (battle) => {
+    setBattleName(battle.name)
+
+    try {
+      await contract.joinBattle()
+
+      setShowAlert({status: true, type: "success", message: `Joining ${battle.name}`})
+    } catch (error) {
+      console.log("Error joining battle", error);
+    }
+  }
 
   return (
     <>
       <h2 className={styles.joinHeadText}>Available Battles:</h2>
+
+      <div className={styles.joinContainer}>
+        {gameState.pendingBattles?.length ? (gameState.pendingBattles.filter((battle) => !battle.players.includes(walletAddress)).map(
+          (battle, index) => (<div key={battle.name} className={styles.flexBetween}>
+            <p className={styles.joinBattleTitle}>
+              {index}. {battle.name}
+            </p>
+            <CustomButton title="Join" handleClick={() => handleClick(battle)} />
+          </div>)
+        )) : <p></p>}
+      </div>
 
       <p className={styles.infoText} onClick={() => navigate('/create-battle')}>
         Or create a new battle
